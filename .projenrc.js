7f5a9a4d00ee184fdf7df7f4da6859c20d52509c
Eli Polonsky <epolon@amazon.com>
2020-06-03 14:37:44 +0300

    add rule to ignore and added constructs dependency

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,7 @@
 const { JsiiProject, Semver } = require('projen');
 
+const constructsDependency = Semver.pinned('2.0.1')
+
 const project = new JsiiProject({
   name: 'stdk8s',
   commitPackageJson: true,
@@ -8,14 +10,25 @@ const project = new JsiiProject({
   repository: 'https://github.com/iliapolo/stdk8s.git',
   authorName: 'Eli Polonsky',
   authorEmail: 'epolon@amazon.com',
+  dependencies: {
+    constructs: constructsDependency,
+  },
+  peerDependencies: {
+    constructs: constructsDependency,
+  },
+  devDependencies: {
+    'cdk8s-cli': Semver.caret('0.21.0')
+  },
   stability: 'experimental',
   workflowBootstrapSteps: [
     { run: `yarn bootstrap` }
-  ]
+  ],
 });
 
 project.addScripts({
   bootstrap: 'yarn install && yarn compile && yarn projen'
 });
+
+project.gitignore.exclude('/.vscode/', '/package-lock.json')
 
 project.synth();
