4e4212cd7020dad0740fa9df55a61c5f0625aa37
Eli Polonsky <epolon@amazon.com>
2020-06-03 13:05:31 +0300

    Initial project generation

---
+++ .projenrc.js
@@ -0,0 +1,20 @@
+const { JsiiProject, Semver } = require('projen');
+
+const project = new JsiiProject({
+  name: 'stdk8s',
+  jsiiVersion: Semver.caret('1.5.0'),
+  description: 'High level abstractions on top of cdk8s',
+  repository: 'https://github.com/iliapolo/stdk8s.git',
+  authorName: 'Eli Polonsky',
+  authorEmail: 'epolon@amazon.com',
+  stability: 'experimental',
+  workflowBootstrapSteps: [
+    { run: `yarn bootstrap` }
+  ]
+});
+
+project.addScripts({
+  bootstrap: 'yarn install && yarn compile && yarn projen'
+});
+
+project.synth();
