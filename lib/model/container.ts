7756433b4987babcf4120be342f09ff538400d85
Elad Ben-Israel <benisrae@amazon.com>
2020-06-11 14:19:17 +0300

    feat: additional capabilities for "PodSpec" and "Container"
    Container:
    - make `name` optional (default to "main").
    - make `port` optional (default to no port is exposed)
    - Add `command`
    
    PodSpec:
    - add `restartPolicy`

--- lib/model/container.ts
+++ lib/model/container.ts
@@ -1,14 +1,23 @@
 import * as model from '../model';
 
 export interface ContainerProps {
-
   readonly image: string;
 
-  readonly name: string;
+  /**
+   * @default "main"
+   */
+  readonly name?: string;
 
-  // TODO: make this an array of structs (see k8s#ContainerPort)
-  readonly port: number
+  /**
+   * // TODO: make this an array of structs (see k8s#ContainerPort)
+   * @default - on port is exposed
+   */
+  readonly port?: number
 
+  /**
+   * The command to execute
+   */
+  readonly command?: string[];
 }
 
 export class Container {
@@ -16,16 +25,17 @@ export class Container {
   public readonly volumeMounts: model.VolumeMount[] = [];
   public readonly name: string;
   public readonly image: string;
-  public readonly port: number;
+  public readonly port?: number;
+  public readonly command?: string[];
 
   constructor(props: ContainerProps) {
-    this.name = props.name;
+    this.name = props.name ?? 'main';
     this.image = props.image;
     this.port = props.port;
+    this.command = props.command;
   }
 
   public mount(options: model.VolumeMount) {
     this.volumeMounts.push(options);
   }
-
 }
