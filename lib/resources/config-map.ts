fe044c18adc26348c92b863781026a0ef05f08c7
Elad Ben-Israel <benisrae@amazon.com>
2020-06-11 20:21:13 +0300

    allow adding files/directories to config maps

--- lib/resources/config-map.ts
+++ lib/resources/config-map.ts
@@ -1,8 +1,9 @@
 import { Construct, Lazy } from 'constructs';
 import * as k8s from '../../imports/k8s';
-import * as model from '../model';
 import { ResourceProps, Resource, IResource } from './base';
 import * as cdk8s from 'cdk8s';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export interface ConfigMapProps extends ResourceProps {
 
@@ -23,14 +24,14 @@ export class ConfigMap extends Resource implements IConfigMap {
 
   public readonly apiObject: cdk8s.ApiObject;
 
-  private readonly binaryData?: { [key: string]: string };
-  private readonly data?: { [key: string]: string };
+  private readonly binaryData: { [key: string]: string };
+  private readonly data: { [key: string]: string };
 
-  public constructor(scope: Construct, id: string, props: ConfigMapProps) {
+  public constructor(scope: Construct, id: string, props: ConfigMapProps = { }) {
     super(scope, id, props);
 
-    this.binaryData = props.binaryData;
-    this.data = props.data;
+    this.binaryData = props.binaryData ?? { };
+    this.data = props.data ?? { };
 
     this.apiObject = new k8s.ConfigMap(this, 'ConfigMap', {
       metadata: {
@@ -40,26 +41,22 @@ export class ConfigMap extends Resource implements IConfigMap {
       data: (Lazy.anyValue({ produce: () => this.data }) as unknown) as Record<string, string>,
       binaryData: (Lazy.anyValue({ produce: () => this.binaryData }) as unknown) as Record<string, string>,
     })
-
   }
 
-  public static fromDirectory(scope: Construct, id: string, directory: string): ConfigMap {
-
-    function readDirectory(): Record<string, string> {
-      throw new Error(`No implemented! (${directory})`);
-    }
-
-    function name(): string {
-      throw new Error('No implemented!');
-    }
-
-    return new ConfigMap(scope, id, {
-      data: readDirectory(),
-      metadata: new model.ObjectMeta({
-        name: name(),
-      }),
-    })
+  public addFile(localFile: string, key?: string) {
+    key = key ?? path.basename(localFile);
+    const value = fs.readFileSync(localFile, 'utf-8');
+    this.data[key] = value;
   }
 
-
+  public addDirectory(localDir: string, keyPrefix: string = '') {
+    for (const file of fs.readdirSync(localDir)) {
+      const localFile = path.join(localDir, file);
+      if (fs.statSync(localFile).isDirectory()) {
+        this.addDirectory(localFile, file + '/');
+      } else {
+        this.addFile(localFile, `${keyPrefix}${localFile}`);
+      }
+    }
+  }
 }
