a00c90ba56dc17abca59162aedff36719fd3cb63
Eli Polonsky <epolon@amazon.com>
2020-06-16 23:50:32 +0300

    option to use env variable as secret

--- lib/secret.ts
+++ lib/secret.ts
@@ -2,6 +2,8 @@ import { IResource, Resource } from './base';
 import * as cdk8s from 'cdk8s';
 import { Construct } from 'constructs';
 import * as k8s from '../imports/k8s';
+import { onSynth } from './utils';
+import { EnvValue } from './container';
 
 export interface SecretProps {
 
@@ -15,6 +17,8 @@ export class Secret extends Resource implements ISecret {
 
   public readonly apiObject: cdk8s.ApiObject;
 
+  private readonly stringData: { [key: string]: string };
+
   public static fromSecretName(name: string): ISecret {
     return { name };
   }
@@ -22,8 +26,22 @@ export class Secret extends Resource implements ISecret {
   public constructor(scope: Construct, id: string, props: SecretProps = { }) {
     super(scope, id, props);
 
-    this.apiObject = new k8s.Secret(this, 'Secret', {})
+    this.stringData = {};
+
+    this.apiObject = new k8s.Secret(this, 'Secret', {
+      metadata: {
+        name: this.metadata?.name,
+        ...this.metadata?._toKube(),
+      },
+      stringData: onSynth(() => this.stringData),
+    })
+  }
+
+  public addEnvVariable(key: string) {
+    const value = EnvValue.fromProcess({key: key, required: true}).value!;
+    this.stringData[key] = value;
   }
 
 }
+
 
